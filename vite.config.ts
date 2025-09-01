8d8b1a26c7d998298d174ef04bdda96d318d0530
tnSiddiq2025 <tn.siddiq2025@gmail.com>
2025-09-01 22:58:03 +0800

    commit 2 publish

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: mode === "production" ? "/sid-glassy-showcase/" : "/", // ✅ repo name
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
